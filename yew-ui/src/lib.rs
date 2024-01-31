37c28c2f9f46d8ba2f18db152aa9a7e057ca5eea
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-01-31 11:16:52 +0900

    OK

--- yew-ui/src/lib.rs
+++ yew-ui/src/lib.rs
@@ -1,5 +1,31 @@
 use yew::prelude::*;
 
+struct TodoView {}
+
+#[derive(Debug, Clone, PartialEq, yew::Properties)]
+struct TodoProp {
+    id: defs::Id,
+    todo: defs::Todo,
+}
+
+enum TodoMsg {}
+
+impl Component for TodoView {
+    type Message = TodoMsg;
+    type Properties = TodoProp;
+    fn create(ctx: &Context<Self>) -> Self {
+        Self {}
+    }
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let todo = ctx.props();
+        if todo.todo.0 {
+            html! {format!("o {:?}", todo.todo.1)}
+        } else {
+            html! {format!("x {:?}", todo.todo.1)}
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, yew::Properties)]
 struct View {
     vec: Vec<(defs::Id, defs::Todo)>,
